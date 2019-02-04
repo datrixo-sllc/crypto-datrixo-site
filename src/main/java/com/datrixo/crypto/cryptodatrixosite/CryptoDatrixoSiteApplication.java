3f3ef985af7f2b13ad2504c2dd3815a1e0db1a54
Yuri Nikiforov <yuri1959@gmail.com>
2019-02-04 15:32:17 +0300

    first

---
+++ src/main/java/com/datrixo/crypto/cryptodatrixosite/CryptoDatrixoSiteApplication.java
@@ -0,0 +1,14 @@
+package com.datrixo.crypto.cryptodatrixosite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoDatrixoSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoDatrixoSiteApplication.class, args);
+	}
+
+}
+
