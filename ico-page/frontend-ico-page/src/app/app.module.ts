b0559e9b7d580ab898c02bd81a46cb619ac0fe0e
Yuri Nikiforov <yuri1959@gmail.com>
2019-02-05 15:57:41 +0300

    update TOTAL SUPPLY / SOLD dinamically add cors disable FOR TEST

--- ico-page/frontend-ico-page/src/app/app.module.ts
+++ ico-page/frontend-ico-page/src/app/app.module.ts
@@ -2,15 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { APP_CONFIG, AppConfig } from './app.config';
+import { HttpClientModule } from '@angular/common/http';
+import {HoldersService} from './holders.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    { provide: APP_CONFIG, useValue: AppConfig },
+    HoldersService
+    ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ ico-page/src/main/java/com/datrixo/crypto_datrixo_site/ico_page/conf/MyConfiguration.java
@@ -0,0 +1,28 @@
+package com.datrixo.crypto_datrixo_site.ico_page.conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by Yuri Nikiforov.
+ * Date: 05.02.2019
+ * Time: 15:49
+ */
+
+@Configuration
+public class MyConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+            }
+        };
+    }
+}
