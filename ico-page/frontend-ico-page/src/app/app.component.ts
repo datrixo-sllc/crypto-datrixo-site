46503d063521b0c695f62f6d8b1ded874c23bcf1
Yuri Nikiforov <yuri1959@gmail.com>
2019-02-05 15:57:41 +0300

    update TOTAL SUPPLY / SOLD dinamically add cors disable FOR TEST

--- ico-page/frontend-ico-page/src/app/app.component.ts
+++ ico-page/frontend-ico-page/src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {interval, Subscription} from 'rxjs';
+import {HoldersService} from './holders.service';
+import {switchMap} from 'rxjs/operators';
+import {IcoPageResponce} from './ico-page-responce';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'frontend-ico-page';
+export class AppComponent implements OnInit, OnDestroy {
+  totalSupply: string;
+  sold: string;
+  responce: IcoPageResponce;
+
+  subscriber: Subscription;
+
+  constructor(private holdersService: HoldersService) {}
+
+  ngOnInit(): void {
+    this.unSubscribe();
+    this.subscriber = interval(1500).pipe(
+      switchMap(() => this.holdersService.getIcoPage())
+    ).subscribe(value => {
+      if (value) {
+        this.responce = value as IcoPageResponce;
+        this.fillValues();
+      }
+    }, error => this.unSubscribe());
+
+  }
+
+  ngOnDestroy(): void {
+    this.unSubscribe();
+
+  }
+
+  unSubscribe(): void {
+    if (this.subscriber) {
+      this.subscriber.unsubscribe();
+    }
+  }
+
+
+  private fillValues() {
+    if (this.responce) {
+      this.totalSupply = this.responce.totalSupplyTokens;
+      this.sold = this.responce.soldTokens;
+    }
+  }
 }
---
+++ ico-page/frontend-ico-page/src/app/app.config.ts
@@ -0,0 +1,9 @@
+import {InjectionToken} from '@angular/core';
+import {IAppConfig} from './i-app-config';
+
+export let APP_CONFIG = new InjectionToken<IAppConfig>('app.config');
+
+export const AppConfig: IAppConfig = {
+    apiEndpoint: 'http://localhost:8080/'
+    /*apiEndpoint: '/'*/
+};
