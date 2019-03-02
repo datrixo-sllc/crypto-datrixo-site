613a283f9fa602c3eb9ac40c0f6e36ca87e0b9de
Yuri Nikiforov <yuri1959@gmail.com>
2019-03-02 22:19:33 +0300

    correction layout

--- ico-page/frontend-ico-page/src/app/app.config.ts
+++ ico-page/frontend-ico-page/src/app/app.config.ts
@@ -4,6 +4,6 @@ import {IAppConfig} from './i-app-config';
 export let APP_CONFIG = new InjectionToken<IAppConfig>('app.config');
 
 export const AppConfig: IAppConfig = {
-    /*apiEndpoint: 'http://localhost:8080/'*/
-    apiEndpoint: '/'
+    apiEndpoint: 'http://localhost:8080/'
+    /*apiEndpoint: '/'*/
 };
