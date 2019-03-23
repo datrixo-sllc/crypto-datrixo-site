9cbc98871f092ac7be0052e777af450e1a263a59
Yuri Nikiforov <yuri1959@gmail.com>
2019-03-23 07:43:02 +0300

    First version of contract, compiled

--- datrixo-contract/contracts/DatrixoToken.sol
+++ datrixo-contract/contracts/DatrixoToken.sol
@@ -39,7 +39,6 @@ contract DatrixoToken is SafeMath {
     uint constant public reservedAmount = 20120000000000;
     uint constant public lockedAmount = 15291200000000;
     address public owner;
-    address public ico;
     /* from this time on tokens may be transfered (after ICO) */
     uint public startTime;
     /* tells if tokens have been burned already */
@@ -48,9 +47,16 @@ contract DatrixoToken is SafeMath {
     /* This creates an array with all balances */
 
     /* This balance structure is
-    *  account address -> Date of purchase -> value of sold Tokens
+    *  account address -> value of sold Tokens - balanceOf
+    *  account address -> time of first purchase - firstPurchaseTime
+    *  transfer check firstPurchseTime if 0 - free sale
     */
-    mapping(address => mapping(uint => uint)) public balanceOf;
+    mapping(address => uint) public balanceOf;
+    mapping(address => uint) public firstPurchaseTime;
+    /*
+    * List of shareholders
+    */
+    address[] public shareholders;
 
     /* This allowance structure is
      * seller account -> customer account -> allowance value of Tokens
@@ -59,7 +65,7 @@ contract DatrixoToken is SafeMath {
 
 
     /* This generates a public event on the blockchain that will notify clients */
-    event Transfer(address indexed from, uint purchaseTime, address indexed to, uint transactionTime, uint value);
+    event Transfer(address indexed from, address indexed to, uint value);
     event Approval(address indexed _owner, address indexed spender, uint value);
     event Burned(uint amount);
 
@@ -68,80 +74,90 @@ contract DatrixoToken is SafeMath {
     constructor(address _ownerAddr, uint _startTime) public {
         owner = _ownerAddr;
         startTime = _startTime;
-        balanceOf[owner][startTime] = totalSupply; // Give the owner all initial tokens with date of purchase = startTime
+        balanceOf[owner] = totalSupply; // Give the owner all initial tokens
     }
 
-    /* Send some of your tokens from your purchase with date = _purchaseTime to a given address and register purchase with date = now*/
-    function transfer(uint _purchaseTime, address _to, uint _value) public returns(bool success){
-        uint _transactionTime = now;
-        require(_transactionTime >= startTime); //check if the crowdsale is already over
-        if (msg.sender == owner) {
-            require(safeSub(balanceOf[msg.sender][startTime], _value) >= lockedAmount); // prevent the owner o spending his share of tokens for company, loyalty program and future financing of the company within the first year
-            _purchaseTime = startTime;
-        } else {
-            if (_purchaseTime != 0) {
-                require(_transactionTime > (_purchaseTime + 365 days)); // prevent sale tokens during first year with first purchase. Following purchases have _purchaseTime = 0 and not controlled
-            }
-            _transactionTime = 0;
+    modifier onlyOwner() {
+        require(msg.sender == owner, "You are not contract owner.");
+        _;
+    }
+
+    modifier afterStartTime() {
+        require(now > startTime, "STO is not started.");
+        _;
+    }
+
+    modifier onlyShareholder() {
+        require(balanceOf[msg.sender] > 0, "You are not shareholder.");
+        _;
+    }
+
+    modifier afterFirstYear() {
+        require(firstPurchaseTime[msg.sender] == 0 || firstPurchaseTime[msg.sender] + 365 days >= now, "First year is not expired.");
+        _;
+    }
+
+    /* First send tokens to shareholder by owner*/
+    function firstTransfer(address _to, uint _value) public onlyOwner afterStartTime returns(bool success) {
+        require(_to != address(0), "Target address is 0x0"); // prevent the owner to spending to address 0x0
+        require(balanceOf[_to] == 0, "Target balance not equal 0"); // prevent secondary transfer
+        require(safeSub(balanceOf[msg.sender], _value) >= lockedAmount, "Value more then locked amount"); // prevent the owner to spending his share of tokens for company, loyalty program and future financing of the company within the first year
+        shareholders.push(_to);
+        firstPurchaseTime[_to] = now;
+        return _transfer(_to, _value);
+    }
+
+    /* Send some of your tokens*/
+    function transfer(address _to, uint _value) public onlyShareholder afterFirstYear returns(bool success){
+        require(_to != address(0), "Target address is 0x0"); // prevent the owner to spending to address 0x0
+        require(firstPurchaseTime[_to] == 0, "Target balance has first transfer amount.");
+        if (firstPurchaseTime[msg.sender] > 0) {
+            delete firstPurchaseTime[msg.sender];
         }
-        balanceOf[msg.sender][_purchaseTime] = safeSub(balanceOf[msg.sender][_purchaseTime], _value); // Subtract from the sender purchase with purchase date = _purchaseTime
-        balanceOf[_to][_transactionTime] = safeAdd(balanceOf[_to][_transactionTime], _value); // Add the same to the recipient purchase with _transactionTime
-        emit Transfer(msg.sender, _purchaseTime, _to, _transactionTime, _value); // Notify anyone listening that this transfer took place
+        if (!checkShareholderExist(_to)) {
+            shareholders.push(_to);
+        }
+        return _transfer(_to, _value);
+    }
+
+    function checkShareholderExist(address _addr) internal view returns(bool success) {
+        for (uint i = 1; i < shareholders.length; i++) {
+            if (shareholders[i] == _addr) return true;
+        }
+        return false;
+    }
+
+    function _transfer(address _to, uint _value) internal returns(bool success){
+        balanceOf[msg.sender] = safeSub(balanceOf[msg.sender], _value); // Subtract from the sender
+        balanceOf[_to] = safeAdd(balanceOf[_to], _value); // Add the same to the recipient purchase with _transactionTime
+        emit Transfer(msg.sender, _to, _value); // Notify anyone listening that this transfer took place
         return true;
     }
 
-    /* Allow another contract or person to spend some tokens in your behalf */
-    function approve(address _spender, uint _value) public returns(bool success) {
-        return _approve(_spender, _value);
-    }
-
-    /* internal approve functionality. needed, so we can check the payloadsize if called externally, but smaller
-     * payload allowed internally */
-    function _approve(address _spender, uint _value) internal returns(bool success) {
-        //  https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
-        require((_value == 0) || (allowance[msg.sender][_spender] == 0));
-        allowance[msg.sender][_spender] = _value;
-        emit Approval(msg.sender, _spender, _value);
-        return true;
-    }
 
 
-    /* to be called when ICO is closed. burns the remaining tokens except the company share (60360000), the tokens reserved
+    /* to be called when STO is closed. burns the remaining tokens except the company share (60360000), the tokens reserved
      * for the bounty/advisors/marketing program (48288000), for the loyalty program (52312000) and for future financing of the company (40240000).
      * anybody may burn the the tokens after ICO ended, but only once (in case the owner holds more tokens in the future).
      * this ensures that the owner will not posses a majority of the tokens. */
-    function burn() public {
-        // if token have not been burned already and the ICO ended
-        require(msg.sender == owner);
-        if (!burned && now > startTime) {
-            uint difference = safeSub(balanceOf[owner][startTime], reservedAmount);
-            balanceOf[owner][startTime] = reservedAmount;
-            totalSupply = safeSub(totalSupply, difference);
-            burned = true;
-            emit Burned(difference);
-        }
+    function burn() public onlyOwner afterStartTime {
+        // if token have not been burned already and the STO ended
+        require(!burned, "Token have been burned already.");
+        uint difference = safeSub(balanceOf[owner], reservedAmount);
+        balanceOf[owner] = reservedAmount;
+        totalSupply = safeSub(totalSupply, difference);
+        burned = true;
+        emit Burned(difference);
+
     }
 
-
     /**
-     * sets the ico address and give it allowance to spend the crowdsale tokens. Only collable once.
-     * @param _icoAddress the address of the ico contract
-     * value the max amount of tokens to sell during the ICO
-     **/
-    function setICO(address _icoAddress) public {
-        require(msg.sender == owner);
-        ico = _icoAddress;
-        assert((_approve(ico, tokensForIco)));
-    }
-
-
-    /**
-     * Allows the ico contract to set the traiding start time to an earler point of time.
+     * Allows the sto contract to set the traiding start time to an earler point of time.
      * (In case the soft cap has been reached)
      * @param _newStart the new start date
      **/
-    function setStart(uint _newStart) public {
-        require(msg.sender == owner && _newStart < startTime);
+    function setStart(uint _newStart) public onlyOwner {
+        require(_newStart < startTime, "New start time must be earlier current start time.");
         startTime = _newStart;
     }
 
