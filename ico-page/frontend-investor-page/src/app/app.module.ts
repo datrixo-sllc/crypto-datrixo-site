45a14a5090751c5a64a3ebda978b97579ed4758f
Yuri Nikiforov <yuri1959@gmail.com>
2019-04-11 07:51:58 +0300

    initialize investor page

--- ico-page/frontend-investor-page/src/app/app.module.ts
+++ ico-page/frontend-investor-page/src/app/app.module.ts
@@ -2,15 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { APP_CONFIG, AppConfig } from './app.config';
+import { HttpClientModule } from '@angular/common/http';
+import {HoldersService} from './holders.service';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import {HoldersDatatableComponent} from './holders-datatable/holders-datatable.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HoldersDatatableComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    NgxDatatableModule
   ],
-  providers: [],
+  providers: [
+    { provide: APP_CONFIG, useValue: AppConfig },
+    HoldersService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ ico-page/frontend-investor-page/src/app/holder-responce.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by Yuri Nikiforov.
+ * Date: 05.02.2019
+ * Time: 14:48
+ */
+export class HolderResponce {
+  address: string;
+  timeDate: Date;
+  shareTokens: string;
+}
