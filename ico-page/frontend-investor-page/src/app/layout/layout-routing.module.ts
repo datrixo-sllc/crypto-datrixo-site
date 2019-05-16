50414d4b203fc45d2ab06a97096d3fba9750e741
Yuri Nikiforov <yuri1959@gmail.com>
2019-05-16 20:33:38 +0300

    investor-profile layout

--- ico-page/frontend-investor-page/src/app/layout/layout-routing.module.ts
+++ ico-page/frontend-investor-page/src/app/layout/layout-routing.module.ts
@@ -7,8 +7,8 @@ const routes: Routes = [
         path: '',
         component: LayoutComponent,
         children: [
-            { path: '', redirectTo: 'dashboard', pathMatch: 'prefix' },
-            { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardModule' },
+            { path: '', redirectTo: 'investor-profile', pathMatch: 'prefix' },
+            { path: 'investor-profile', loadChildren: './investor-profile/investor-profile.module#InvestorProfileModule' },
             { path: 'charts', loadChildren: './charts/charts.module#ChartsModule' },
             { path: 'tables', loadChildren: './tables/tables.module#TablesModule' },
             { path: 'forms', loadChildren: './form/form.module#FormModule' },
