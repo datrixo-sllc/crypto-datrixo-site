42ee789294b2b64d794ce8e5979103d77b8ac901
Yuri Nikiforov <yuri1959@gmail.com>
2019-05-20 07:48:59 +0300

    spring security without client part

---
+++ ico-page/src/main/java/com/datrixo/crypto_datrixo_site/ico_page/service/UserService.java
@@ -0,0 +1,14 @@
+package com.datrixo.crypto_datrixo_site.ico_page.service;
+
+import com.datrixo.crypto_datrixo_site.ico_page.mysql.model.User;
+
+import java.util.Optional;
+
+/**
+ * Created by Yuri Nikiforov.
+ * Date: 20.05.2019
+ * Time: 7:28
+ **/
+public interface UserService {
+    Optional<User> findByEmail(String email);
+}
