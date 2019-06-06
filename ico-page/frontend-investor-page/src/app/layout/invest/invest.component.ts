abb3c97168cc4d95a9c8c4cb013c978ba07abbcd
Yuri Nikiforov <yuri1959@gmail.com>
2019-06-06 21:24:41 +0300

    holders datatable

--- ico-page/frontend-investor-page/src/app/layout/invest/invest.component.ts
+++ ico-page/frontend-investor-page/src/app/layout/invest/invest.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {routerTransition} from '../../router.animations';
 import {InvestDownloadService} from './invest-download.service';
 import {Response} from '@angular/http';
@@ -9,6 +9,7 @@ import {InvestService} from './invest.service';
 import {HolderResponce} from './holder-responce';
 import {IcoPageResponse} from './ico-page-response';
 import {interval, Subscription} from 'rxjs';
+import {switchMap} from 'rxjs/internal/operators/switchMap';
 
 @Component({
     selector: 'app-invest',
@@ -16,7 +17,7 @@ import {interval, Subscription} from 'rxjs';
     styleUrls: ['./invest.component.scss'],
     animations: [routerTransition()]
 })
-export class InvestComponent implements OnInit {
+export class InvestComponent implements OnInit, OnDestroy {
     private static readonly  FN_PPM: string = 'ppm.pdf';
     fileToUpload: File = null;
     @ViewChild('uploadFile') uploadEl: ElementRef;
@@ -36,7 +37,18 @@ export class InvestComponent implements OnInit {
         private spinner: NgxSpinnerService,
         private recieveUtils: RecieveUtils) {}
 
-    ngOnInit() {}
+    ngOnInit() {
+        this.getIcoPage();
+        this.unSubscribe();
+        this.subscriber = interval(300000/*5 min*/).pipe(
+            switchMap(() => this.investService.getIcoPage())
+        ).subscribe(value => {
+            if (value) {
+                this.response = value as IcoPageResponse;
+                this.fillValues();
+            }
+        }, error => this.unSubscribe());
+    }
 
     onNavigate() {
         window.open('http://datrixo.com', '_blank');
@@ -88,4 +100,34 @@ export class InvestComponent implements OnInit {
     }
 
 
+    getIcoPage(): void {
+        this.investService.getIcoPage()
+            .subscribe(value => {
+                if (value) {
+                    this.response = value as IcoPageResponse;
+                    this.fillValues();
+                }
+            });
+    }
+
+    ngOnDestroy(): void {
+        this.unSubscribe();
+
+    }
+
+    unSubscribe(): void {
+        if (this.subscriber) {
+            this.subscriber.unsubscribe();
+        }
+    }
+
+
+    private fillValues() {
+        if (this.response) {
+            this.totalSupply = this.response.totalSupplyTokens;
+            this.sold = this.response.soldTokens;
+            this.holdersCount = this.response.holdersCount;
+            this.holders = this.response.holders;
+        }
+    }
 }
