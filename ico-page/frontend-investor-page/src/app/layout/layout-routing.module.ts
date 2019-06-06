1612be19ffeb1ab001f2bfb9029da0b212425ea9
Yuri Nikiforov <yuri1959@gmail.com>
2019-06-06 21:56:55 +0300

    my-holdings

--- ico-page/frontend-investor-page/src/app/layout/layout-routing.module.ts
+++ ico-page/frontend-investor-page/src/app/layout/layout-routing.module.ts
@@ -10,12 +10,8 @@ const routes: Routes = [
             { path: '', redirectTo: 'investor-profile' },
             { path: 'investor-profile', loadChildren: './investor-profile/investor-profile.module#InvestorProfileModule' },
             { path: 'invest', loadChildren: './invest/invest.module#InvestModule' },
-            { path: 'tables', loadChildren: './tables/tables.module#TablesModule' },
-            { path: 'forms', loadChildren: './form/form.module#FormModule' },
-            { path: 'bs-element', loadChildren: './bs-element/bs-element.module#BsElementModule' },
-            { path: 'grid', loadChildren: './grid/grid.module#GridModule' },
-            { path: 'components', loadChildren: './bs-component/bs-component.module#BsComponentModule' },
-            { path: 'blank-page', loadChildren: './blank-page/blank-page.module#BlankPageModule' }
+            { path: 'my-holdings', loadChildren: './my-holdings/my-holdings.module#MyHoldingsModule' }
+
         ]
     }
 ];
---
+++ ico-page/frontend-investor-page/src/app/layout/my-holdings/my-holdings.component.ts
@@ -0,0 +1,71 @@
+import {Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {routerTransition} from '../../router.animations';
+import {Response} from '@angular/http';
+import { NgxSpinnerService } from 'ngx-spinner';
+import {MyHoldingsService} from './my-holdings.service';
+import {HolderResponce} from './holder-responce';
+import {IcoPageResponse} from './ico-page-response';
+import {interval, Subscription} from 'rxjs';
+import {switchMap} from 'rxjs/internal/operators/switchMap';
+
+@Component({
+    selector: 'app-my-holdings',
+    templateUrl: './my-holdings.component.html',
+    styleUrls: ['./my-holdings.component.scss'],
+    animations: [routerTransition()]
+})
+export class MyHoldingsComponent implements OnInit, OnDestroy {
+
+    holders: HolderResponce[];
+    response: IcoPageResponse;
+
+    subscriber: Subscription;
+
+    constructor(
+        private myHoldingsService: MyHoldingsService,
+        private spinner: NgxSpinnerService) {}
+
+    ngOnInit() {
+        this.getIcoPage();
+        this.unSubscribe();
+        this.subscriber = interval(300000/*5 min*/).pipe(
+            switchMap(() => this.myHoldingsService.getIcoPage())
+        ).subscribe(value => {
+            if (value) {
+                this.response = value as IcoPageResponse;
+                this.fillValues();
+            }
+        }, error => this.unSubscribe());
+    }
+
+
+
+
+    getIcoPage(): void {
+        this.myHoldingsService.getIcoPage()
+            .subscribe(value => {
+                if (value) {
+                    this.response = value as IcoPageResponse;
+                    this.fillValues();
+                }
+            });
+    }
+
+    ngOnDestroy(): void {
+        this.unSubscribe();
+
+    }
+
+    unSubscribe(): void {
+        if (this.subscriber) {
+            this.subscriber.unsubscribe();
+        }
+    }
+
+
+    private fillValues() {
+        if (this.response) {
+            this.holders = this.response.holders;
+        }
+    }
+}
