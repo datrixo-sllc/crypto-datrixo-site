c8f0658ce9b77faf37d96b0f78e6f859a87d1d2d
Yuri Nikiforov <yuri1959@gmail.com>
2019-06-06 09:06:40 +0300

    holders datatable

---
+++ ico-page/frontend-investor-page/src/app/layout/invest/ico-page-response.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by Yuri Nikiforov.
+ * Date: 05.02.2019
+ * Time: 14:46
+ */
+import {HolderResponce} from './holder-responce';
+
+export class IcoPageResponse {
+  totalSupplyTokens: string;
+  soldTokens: string;
+  holdersCount: string;
+  holders: HolderResponce[];
+}
