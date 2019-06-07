197b3b304d64670f1eab309bac3f668bdbf0824b
Yuri Nikiforov <yuri1959@gmail.com>
2019-06-07 07:53:40 +0300

    add share column

--- ico-page/src/main/java/com/datrixo/crypto_datrixo_site/ico_page/service/IcoPageServiceImpl.java
+++ ico-page/src/main/java/com/datrixo/crypto_datrixo_site/ico_page/service/IcoPageServiceImpl.java
@@ -32,7 +32,8 @@ public class IcoPageServiceImpl implements IcoPageService {
         icoPageDto.setHoldersCount(String.valueOf(holders.size()));
         List<HolderDto> holderDtoList = new ArrayList<>();
         holders.forEach(holder -> {
-            HolderDto holderDto = new HolderDto(holder.getAddress(), holder.getTimeDate(), String.valueOf(holder.getShareTokens()));
+            HolderDto holderDto = new HolderDto(holder.getAddress(), holder.getTimeDate(),
+                    String.valueOf(holder.getShareTokens()), String.valueOf(holder.getShare()));
             holderDtoList.add(holderDto);
         });
         icoPageDto.setHolders(holderDtoList);
