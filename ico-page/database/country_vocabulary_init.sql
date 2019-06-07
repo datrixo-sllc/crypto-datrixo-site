2f4c5e62ecf1ba4f7ea95088e9abe37b89f5a588
Yuri Nikiforov <yuri1959@gmail.com>
2019-06-07 10:52:34 +0300

    user model

--- ico-page/database/country_vocabulary_init.sql
+++ ico-page/database/country_vocabulary_init.sql
@@ -1,1 +1,4 @@
-insert into country(id, name, code) values (1, "Aland Islands", "AX");
+insert into country(id, name, code) values (1, "France", "FR");
+insert into country(id, name, code) values (2, "Germany", "DE");
+insert into country(id, name, code) values (3, "United Kingdom", "GB");
+insert into country(id, name, code) values (4, "United States", "US");
