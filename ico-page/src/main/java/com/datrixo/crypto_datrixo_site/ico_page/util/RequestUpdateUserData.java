03f21b5920aba5108e44d354e99b6223d4b1ee02
Yuri Nikiforov <yuri1959@gmail.com>
2019-06-09 14:33:34 +0300

    user profile without backend

---
+++ ico-page/src/main/java/com/datrixo/crypto_datrixo_site/ico_page/util/RequestUpdateUserData.java
@@ -0,0 +1,45 @@
+package com.datrixo.crypto_datrixo_site.ico_page.util;
+
+/**
+ * Created by Yuri Nikiforov.
+ * Date: 09.06.2019
+ * Time: 14:20
+ **/
+public class RequestUpdateUserData {
+    private String title;
+    private String firstName;
+    private String lastName;
+    private String phone;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
