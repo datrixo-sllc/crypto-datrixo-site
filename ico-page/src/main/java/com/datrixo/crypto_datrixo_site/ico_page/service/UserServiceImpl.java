8494e551031cc8be1c39f6e135e27fb0e6bb5ba3
Yuri Nikiforov <yuri1959@gmail.com>
2019-06-09 14:33:34 +0300

    user profile without backend

--- ico-page/src/main/java/com/datrixo/crypto_datrixo_site/ico_page/service/UserServiceImpl.java
+++ ico-page/src/main/java/com/datrixo/crypto_datrixo_site/ico_page/service/UserServiceImpl.java
@@ -1,10 +1,18 @@
 package com.datrixo.crypto_datrixo_site.ico_page.service;
 
 import com.datrixo.crypto_datrixo_site.ico_page.mysql.model.User;
+import com.datrixo.crypto_datrixo_site.ico_page.mysql.model.util.UserTitle;
 import com.datrixo.crypto_datrixo_site.ico_page.mysql.repository.UserRepository;
+import com.datrixo.crypto_datrixo_site.ico_page.security.MediUser;
+import com.datrixo.crypto_datrixo_site.ico_page.util.RequestUpdateUserData;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.nio.file.attribute.UserPrincipalNotFoundException;
 import java.util.Optional;
 
 /**
@@ -20,5 +28,22 @@ public class UserServiceImpl implements UserService {
     @Override
     public Optional<User> findByUsername(String username) {
         return userRepository.findByUsername(username);
+    }
+    @Transactional
+    public void updateUser(RequestUpdateUserData updateUserData) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        MediUser currentUser = (MediUser)auth.getPrincipal();
+        Optional<User> optionalUser = findByUsername(currentUser.getUsername());
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            user.setTitle(UserTitle.valueOf(updateUserData.getTitle()));
+            user.setFirstName(updateUserData.getFirstName());
+            user.setLastName(updateUserData.getLastName());
+            user.setPhone(updateUserData.getPhone());
+
+            userRepository.saveAndFlush(user);
+        } else {
+            throw new UsernameNotFoundException("user not found");
+        }
     }
 }
