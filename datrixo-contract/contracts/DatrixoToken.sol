d11df13e9693ababa7ff888108ece32ade22384d
Yuri Nikiforov <yuri1959@gmail.com>
2019-12-27 18:38:36 +0300

    new datrixo contract DRXT

--- datrixo-contract/contracts/DatrixoToken.sol
+++ datrixo-contract/contracts/DatrixoToken.sol
@@ -35,7 +35,7 @@ contract DatrixoToken is SafeMath {
     string constant public name = "DatrixoToken";
     string constant public symbol = "DRXT";
     uint8 constant public decimals = 5;
-    uint public totalSupply = 8000000;
+    uint public totalSupply = 16000000;
 
     address public owner;
     /* from this time on tokens may be transferred (after ICO) */
