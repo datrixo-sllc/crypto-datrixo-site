092f814e719696bae07160b2f52b6c2aab1d27de
Yuri Nikiforov <yuri1959@gmail.com>
2020-01-13 01:30:02 +0300

    js file for datrixo.com wordpress

--- sto-page/holders.js
+++ sto-page/holders.js
@@ -1,7 +1,23 @@
 jQuery(document).ready( function() {
 	
-	document.getElementById("totalSupply").textContent="160";
-	document.getElementById("sold").textContent="24";
-	
-	jQuery('#tableHolders').DataTable();
+	jQuery.getJSON('http://ico-page-site-backend.gned73gxqz.us-east-1.elasticbeanstalk.com/ico/ico-page', function (data) {
+
+		document.getElementById("totalSupply").textContent = data.totalSupplyTokens;
+		document.getElementById("sold").textContent = data.soldTokens;
+		document.getElementById("holdersCount").textContent = data.holdersCount;
+		var holders = [];
+		for (ind in data.holders) {
+			var item = [];
+			item.push("<a href='https://ropsten.etherscan.io/address/" + data.holders[ind].address +
+				"#readContract' target='_blank'>" + data.holders[ind].address + "</a>");
+			item.push(data.holders[ind].timeDate);
+			item.push(data.holders[ind].shareTokens);
+			item.push(data.holders[ind].share);
+			holders.push(item);
+
+		}
+		console.log(holders);
+
+		jQuery('#tableHolders').DataTable({data: holders});
+	});
 });
