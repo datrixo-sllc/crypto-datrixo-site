444579029467781054a7ed6676f0635369816736
Yuri Nikiforov <yuri1959@gmail.com>
2020-01-12 13:07:33 +0300

    js file for datrixo.com wordpress

---
+++ sto-page/holders.js
@@ -0,0 +1,7 @@
+jQuery(document).ready( function() {
+	
+	document.getElementById("totalSupply").textContent="160";
+	document.getElementById("sold").textContent="24";
+	
+	jQuery('#tableHolders').DataTable();
+});
