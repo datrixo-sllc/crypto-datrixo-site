ea55432a50485736749046bd44ca0f6edaa443b2
Yuri Nikiforov <yuri1959@gmail.com>
2020-04-24 14:03:49 +0300

    new text for sto page

--- ico-page/frontend-ico-page/src/app/app.config.ts
+++ ico-page/frontend-ico-page/src/app/app.config.ts
@@ -5,5 +5,5 @@ export let APP_CONFIG = new InjectionToken<IAppConfig>('app.config');
 
 export const AppConfig: IAppConfig = {
     /*apiEndpoint: 'http://localhost:8080/'*/
-    apiEndpoint: 'http://ico-page-site-backend.gned73gxqz.us-east-1.elasticbeanstalk.com/'
+    apiEndpoint: 'https://api-backend.datrixo.com/'
 };
