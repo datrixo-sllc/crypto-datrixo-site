905d36867b0539e44f1825293848efffc8270689
Yuri Nikiforov <yuri1959@gmail.com>
2020-06-01 20:59:36 +0300

    new holding tables

--- ico-page/src/main/java/com/datrixo/crypto_datrixo_site/ico_page/service/IcoPageServiceImpl.java
+++ ico-page/src/main/java/com/datrixo/crypto_datrixo_site/ico_page/service/IcoPageServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigInteger;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,6 +20,7 @@ import java.util.List;
 public class IcoPageServiceImpl implements IcoPageService {
     @Autowired
     private HolderService holderService;
+    private static DecimalFormat df2 = new DecimalFormat("#.00");
 
     @Override
     public IcoPageDto getAllData() {
@@ -31,13 +33,13 @@ public class IcoPageServiceImpl implements IcoPageService {
                 .reduce(0d, Double::sum);
         IcoPageDto icoPageDto = new IcoPageDto();
         icoPageDto.setTotalSupplyTokens(String.valueOf(holderService.getTotalSupply()));
-        icoPageDto.setSoldEquity(String.valueOf(share));
+        icoPageDto.setSoldEquity(df2.format(share));
         icoPageDto.setSoldTokens(String.valueOf(totalShareTokens));
         icoPageDto.setHoldersCount(String.valueOf(holders.size()));
         List<HolderDto> holderDtoList = new ArrayList<>();
         holders.forEach(holder -> {
             HolderDto holderDto = new HolderDto(holder.getAddress(), holder.getTimeDate(),
-                    String.valueOf(holder.getShareTokens()), String.valueOf(holder.getShare()));
+                    String.valueOf(holder.getShareTokens()), df2.format(holder.getShare()));
             holderDtoList.add(holderDto);
         });
         icoPageDto.setHolders(holderDtoList);
