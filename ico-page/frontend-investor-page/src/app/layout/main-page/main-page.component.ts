0b6a8c6063ebbe47fab728a31b4c8e3b6ff53401
Yuri Nikiforov <yuri1959@gmail.com>
2020-06-04 19:58:14 +0300

    new main page

--- ico-page/frontend-investor-page/src/app/layout/main-page/main-page.component.ts
+++ ico-page/frontend-investor-page/src/app/layout/main-page/main-page.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {routerTransition} from '../../router.animations';
+import {NgxSpinnerService} from 'ngx-spinner';
+import {MainPageService} from './main-page.service';
+import {RespUserMainData} from './resp-user-main-data';
+import * as Noty from 'noty';
 
 @Component({
     selector: 'app-main-page',
@@ -8,7 +12,41 @@ import {routerTransition} from '../../router.animations';
     animations: [routerTransition()]
 })
 export class MainPageComponent implements OnInit {
-    constructor() {}
 
-    ngOnInit() {}
+    userData: RespUserMainData = new RespUserMainData();
+    alertTitle: string;
+    alertBody: string;
+
+    constructor(private spinner: NgxSpinnerService,
+                private mainPageService: MainPageService) {
+    }
+
+    ngOnInit() {
+        this.spinner.show();
+        this.alertTitle = 'Investor Data';
+        this.mainPageService.getUserMainData()
+            .toPromise()
+            .then((value: any) => {
+                    this.userData = value as RespUserMainData;
+                    this.alertBody = 'Successfully loaded';
+                    this.spinner.hide();
+                    this.notyMessage(this.alertTitle, this.alertBody, 'success').show();
+                },
+                (reason: Error) => {
+                    this.alertTitle = 'Investor Data';
+                    this.alertBody = 'Server error: ' + reason;
+                    this.spinner.hide();
+                    this.notyMessage(this.alertTitle, this.alertBody, 'error').show();
+                });
+
+    }
+
+    notyMessage(alertTitle: string, alertBody: string, messageType: Noty.Type): Noty {
+        return new Noty({
+            type: messageType,
+            text: '<strong>' + alertTitle + '</strong><br /> ' + alertBody,
+            timeout: 3000
+        });
+    }
+
 }
---
+++ ico-page/frontend-investor-page/src/app/layout/main-page/resp-user-main-data.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by Yuri Nikiforov.
+ * Date: 03.06.2020
+ * Time: 17:05
+ */
+export class RespUserMainData {
+    totalInvestedInt: number;
+    totalInvestedDec: number;
+    valueEstimateInt: number;
+    valueEstimateDec: number;
+}
