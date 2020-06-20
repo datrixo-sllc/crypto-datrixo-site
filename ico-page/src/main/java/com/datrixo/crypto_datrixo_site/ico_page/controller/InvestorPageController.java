cccd252057cf856833cee6489e7daafc870291bf
Yuri Nikiforov <yuri1959@gmail.com>
2020-06-20 21:10:00 +0300

    total invested for initial persons

--- ico-page/src/main/java/com/datrixo/crypto_datrixo_site/ico_page/controller/InvestorPageController.java
+++ ico-page/src/main/java/com/datrixo/crypto_datrixo_site/ico_page/controller/InvestorPageController.java
@@ -33,6 +33,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigDecimal;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -155,11 +156,13 @@ public class InvestorPageController {
         IcoPageDto icoPageDto = getHoldings();
 
         Integer investedTokens = icoPageDto.getHolders().parallelStream()
-                .reduce(0, (partialResult, holder) -> Integer.valueOf(holder.getPaidPrice()), Integer::sum);
+                .reduce(0, (partialResult, holder) ->
+                        new BigDecimal(holder.getPaidPrice()).intValue() * Integer.valueOf(holder.getShareTokens()),
+                        Integer::sum);
 
         Integer shareTokens = icoPageDto.getHolders().parallelStream()
                 .reduce(0, (partialResult, holder) -> Integer.valueOf(holder.getShareTokens()), Integer::sum);
-        return new UserMainDataDto(investedTokens * UNIT_VALUE, 0,
+        return new UserMainDataDto(investedTokens, 0,
                 shareTokens * UNIT_VALUE, 0);
     }
 
