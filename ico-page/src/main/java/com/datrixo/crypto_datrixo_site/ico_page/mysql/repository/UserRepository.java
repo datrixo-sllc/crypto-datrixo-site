23e818a8e26077061179365f0f5b8778ec3150d5
Yuri Nikiforov <yuri1959@gmail.com>
2021-09-06 16:22:02 +0300

    Query with fetch objects with lazy fetch

--- ico-page/src/main/java/com/datrixo/crypto_datrixo_site/ico_page/mysql/repository/UserRepository.java
+++ ico-page/src/main/java/com/datrixo/crypto_datrixo_site/ico_page/mysql/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.datrixo.crypto_datrixo_site.ico_page.mysql.repository;
 
 import com.datrixo.crypto_datrixo_site.ico_page.mysql.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,4 +17,11 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String email);
+
+    @Query("select u from User u " +
+            "join fetch u.imageContent im " +
+            "join fetch u.organization o " +
+            "join fetch o.country c " +
+            "join fetch u.accounts a")
+    List<User> findAll();
 }
