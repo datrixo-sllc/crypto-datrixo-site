c79d2ba57f7204c6b64ecbc022ddef43449ff08d
Yuri Nikiforov <yuri1959@gmail.com>
2021-09-21 19:48:54 +0300

    stripe

--- ico-page/frontend-investor-page/src/environments/environment.prod.ts
+++ ico-page/frontend-investor-page/src/environments/environment.prod.ts
@@ -1,5 +1,8 @@
 export const environment = {
   production: true,
-    stripe: 'pk_test_51JSlryH9ApoYBcMstUrVkWUhx3WwH8hWV8NO3iZY94qItoYpsvZenxvGzII0ckZWGP1ZxKqoA4ts43zw7H6dCjNs00yLP37Oyy',
+    // Yuri
+    /*stripe: 'pk_test_51JSlryH9ApoYBcMstUrVkWUhx3WwH8hWV8NO3iZY94qItoYpsvZenxvGzII0ckZWGP1ZxKqoA4ts43zw7H6dCjNs00yLP37Oyy',*/
+    // Milla
+    stripe: 'pk_test_51IyIIFEiwPl6jGSOKZSutYRPZMH93irxNqWIRmOeeb7qbLKRUMcdfih5dJ4IJoi8oQor9DGTE6h5J8H3Fvnrcoby00KyA8hoJN',
     serverUrl: 'https://dashboard.datrixo.com/'
 };
