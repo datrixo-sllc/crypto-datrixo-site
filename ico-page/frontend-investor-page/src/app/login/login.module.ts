26892811fddbff41a3607428afe8218d944aaa35
Yuri Nikiforov <yuri1959@gmail.com>
2022-02-05 22:32:31 +0300

    password field with eye

--- ico-page/frontend-investor-page/src/app/login/login.module.ts
+++ ico-page/frontend-investor-page/src/app/login/login.module.ts
@@ -6,13 +6,16 @@ import { FormsModule } from '@angular/forms';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 import {LoginService} from './login.service';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
     imports: [
         CommonModule,
         TranslateModule,
         FormsModule,
-        LoginRoutingModule],
+        LoginRoutingModule,
+        MatIconModule
+    ],
     declarations: [LoginComponent],
     providers: [LoginService]
 })
