bf263bdce31156c66df6a8573dc7bf9824daef8f
Yuri Nikiforov <yuri1959@gmail.com>
2022-02-07 21:41:09 +0300

    new my holdings layout responsive table

--- ico-page/frontend-investor-page/src/app/layout/my-equity-holdings/my-equity-holdings.module.ts
+++ ico-page/frontend-investor-page/src/app/layout/my-equity-holdings/my-equity-holdings.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { MyEquityHoldingsRoutingModule } from './my-equity-holdings-routing.module';
 import { MyEquityHoldingsComponent } from './my-equity-holdings.component';
-import { PageHeaderModule } from './../../shared';
+import { MaterialModule, PageHeaderModule, SharedPipesModule} from './../../shared';
 import { FileSaverModule } from 'ngx-filesaver';
 import {MyEquityHoldingDatatableComponent} from './my-equity-holding-datatable/my-equity-holding-datatable.component';
 import {MyEquityHoldingsService} from './my-equity-holdings.service';
 import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import {
+    MyEquityHoldingDatatableResponsiveComponent
+} from './my-equity-holding-datatable-responsive/my-equity-holding-datatable-responsive.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
     imports: [
@@ -15,11 +19,14 @@ import { NgxDatatableModule } from '@swimlane/ngx-datatable';
         MyEquityHoldingsRoutingModule,
         PageHeaderModule,
         FileSaverModule,
-        NgxDatatableModule
+        NgxDatatableModule,
+        MaterialModule,
+        NgbModule
     ],
     declarations: [
         MyEquityHoldingsComponent,
-        MyEquityHoldingDatatableComponent
+        MyEquityHoldingDatatableComponent,
+        MyEquityHoldingDatatableResponsiveComponent
     ],
     providers: [
         MyEquityHoldingsService
