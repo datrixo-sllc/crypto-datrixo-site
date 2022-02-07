889603f358df236be5506edaff99a276bd02b485
Yuri Nikiforov <yuri1959@gmail.com>
2022-02-07 16:12:05 +0300

    new profile layout responcive table

---
+++ ico-page/frontend-investor-page/src/app/layout/investor-profile/holding-datatable-responsive/holding-datatable-responsive.component.ts
@@ -0,0 +1,72 @@
+/**
+ * Created by Yuri Nikiforov.
+ * Date: 22.07.2021
+ * Time: 21:38
+ */
+
+import {Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges, ViewChild} from '@angular/core';
+import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
+import {DomSanitizer} from '@angular/platform-browser';
+import {Holder} from '../holder';
+
+
+@Component({
+    selector: 'app-holding-datatable-responsive',
+    styleUrls: ['holding-datatable-responsive.component.scss'],
+    templateUrl: 'holding-datatable-responsive.component.html',
+})
+export class HoldingDatatableResponsiveComponent implements OnInit, OnChanges {
+    @Input() items: Holder[];
+    /*@Output() onAddItemEmit = new EventEmitter<string>();
+    @Output() onChangeStatusEmit = new EventEmitter<number>();
+    @Output() onEditItemEmit = new EventEmitter<number>();*/
+
+    etherNet = 'etherscan.io';
+    displayedColumns: string[] = ['address', 'createDate', 'paidPrice', 'initialInvest', 'equityTokens', 'sharePercent'];
+    dataSource = new MatTableDataSource<Holder>();
+    dataSourceLenth: number;
+
+    @ViewChild(MatPaginator) paginator: MatPaginator;
+
+    constructor(public sanitizer: DomSanitizer) {
+    }
+
+    ngOnInit() {
+        this.dataSource = new MatTableDataSource<Holder>(this.items);
+        this.dataSource.paginator = this.paginator;
+        this.dataSourceLenth = this.dataSource.data.length;
+    }
+
+    ngOnChanges(changes: SimpleChanges): void {
+        this.dataSource = new MatTableDataSource<Holder>(this.items);
+        this.dataSource.paginator = this.paginator;
+        this.dataSourceLenth = this.dataSource.data.length;
+    }
+
+    applyFilter(filterValue: string) {
+        this.dataSource.filter = filterValue.trim().toLowerCase();
+
+        if (this.dataSource.paginator) {
+            this.dataSource.paginator.firstPage();
+        }
+    }
+
+    /*onEditItem(id: number) {
+        this.onEditItemEmit.emit(id);
+    }
+
+    onChangeStatus(id: number) {
+        this.onChangeStatusEmit.emit(id);
+
+    }
+
+    onAddItem() {
+        this.onAddItemEmit.emit('addItem');
+    }*/
+
+    /*generateIngredientsString(ingredientPositions: IngredientPosition[]): string {
+        return  ingredientPositions.map(value => value.ingredient.name).join(', ');
+    }*/
+}
+
+
--- ico-page/frontend-investor-page/src/app/layout/investor-profile/investor-profile.module.ts
+++ ico-page/frontend-investor-page/src/app/layout/investor-profile/investor-profile.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { InvestorProfileRoutingModule } from './investor-profile-routing.module';
 import { InvestorProfileComponent } from './investor-profile.component';
-import { PageHeaderModule } from './../../shared';
+import { MaterialModule, PageHeaderModule, SharedPipesModule} from './../../shared';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import {InvestorProfileService} from './investor-profile.service';
 import {FormsModule} from '@angular/forms';
+import {HoldingDatatableResponsiveComponent} from './holding-datatable-responsive/holding-datatable-responsive.component';
 
 @NgModule({
     imports: [
@@ -14,9 +15,11 @@ import {FormsModule} from '@angular/forms';
         FormsModule,
         InvestorProfileRoutingModule,
         PageHeaderModule,
-        NgbModule
+        NgbModule,
+        MaterialModule,
+        SharedPipesModule,
     ],
-    declarations: [InvestorProfileComponent],
+    declarations: [InvestorProfileComponent, HoldingDatatableResponsiveComponent],
     providers: [InvestorProfileService]
 })
 export class InvestorProfileModule {}
