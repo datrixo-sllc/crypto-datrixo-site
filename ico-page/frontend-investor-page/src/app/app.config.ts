e1c9a18cb796f46c55ad3c8c6b7cea313c27f91b
Yuri Nikiforov <yuri1959@gmail.com>
2022-02-07 21:09:33 +0300

    new my drx holdings layout responsive table

--- ico-page/frontend-investor-page/src/app/app.config.ts
+++ ico-page/frontend-investor-page/src/app/app.config.ts
@@ -4,7 +4,7 @@ import {IAppConfig} from './i-app-config';
 export let APP_CONFIG = new InjectionToken<IAppConfig>('app.config');
 
 export const AppConfig: IAppConfig = {
-    /*apiEndpoint: 'http://localhost:8080/'*/
-    apiEndpoint: 'https://api2-backend.datrixo.com/'
+    apiEndpoint: 'http://localhost:8080/'
+    /*apiEndpoint: 'https://api2-backend.datrixo.com/'*/
     /*apiEndpoint: 'http://cryptodatrixosite-env.eba-yk3wcktq.us-east-1.elasticbeanstalk.com/'*/
 };
