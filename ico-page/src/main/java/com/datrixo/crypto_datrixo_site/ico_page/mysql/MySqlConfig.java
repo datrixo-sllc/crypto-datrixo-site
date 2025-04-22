84dd2fcd59a146bca940632863fc23706efb5694
n9357 <n9357@yandex.ru>
2025-04-22 17:36:00 +0300

    javax.sql.DataSource

--- ico-page/src/main/java/com/datrixo/crypto_datrixo_site/ico_page/mysql/MySqlConfig.java
+++ ico-page/src/main/java/com/datrixo/crypto_datrixo_site/ico_page/mysql/MySqlConfig.java
@@ -17,7 +17,7 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import jakarta.persistence.EntityManagerFactory;
-import jakarta.sql.DataSource;
+import javax.sql.DataSource;
 import java.util.Properties;
 
 /**
