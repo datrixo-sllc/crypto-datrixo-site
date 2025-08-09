6555ecc5631bb0f02fac849f50d38b1602be41bb
nik <yuri1959@gmail.com>
2025-08-09 11:54:12 +0300

    prepare for production

--- ico-page/frontend-investor-page-update/src/environments/environment.prod.ts
+++ ico-page/frontend-investor-page-update/src/environments/environment.prod.ts
@@ -4,5 +4,6 @@ export const environment = {
     /*stripe: 'pk_test_51JSlryH9ApoYBcMstUrVkWUhx3WwH8hWV8NO3iZY94qItoYpsvZenxvGzII0ckZWGP1ZxKqoA4ts43zw7H6dCjNs00yLP37Oyy',*/
     // Milla
     stripe: 'pk_test_51IyIIFEiwPl6jGSOKZSutYRPZMH93irxNqWIRmOeeb7qbLKRUMcdfih5dJ4IJoi8oQor9DGTE6h5J8H3Fvnrcoby00KyA8hoJN',
-    serverUrl: 'https://dashboard.datrixo.com/'
+    //serverUrl: 'https://dashboard.datrixo.com/'
+    serverUrl: 'https://dashboard.datrixo.com/drx_investors/'
 };
