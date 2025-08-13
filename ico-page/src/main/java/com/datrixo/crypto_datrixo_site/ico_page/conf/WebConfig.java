f24369da1b0bfc6cab113674c8ee9757b26e210d
datrixo-sllc <datrixosllc@gmail.com>
2025-08-13 19:08:56 +0300

    fix url

---
+++ ico-page/src/main/java/com/datrixo/crypto_datrixo_site/ico_page/conf/WebConfig.java
@@ -0,0 +1,22 @@
+package com.datrixo.crypto_datrixo_site.ico_page.conf;
+
+/**
+ * Created by Yuri Nikiforov.
+ * Date: 13.08.2025
+ * Time: 18:00
+ **/
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        // Перенаправление корня на index.html
+        registry.addViewController("/ico/").setViewName("forward:/ico/index.html");
+        registry.addViewController("/investors/").setViewName("forward:/investors/index.html");
+    }
+}
