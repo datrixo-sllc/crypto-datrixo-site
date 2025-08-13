5cad34ffbee15181747ad049f578bfa88d5e0758
datrixo-sllc <datrixosllc@gmail.com>
2025-08-13 20:29:23 +0300

    fix url

--- ico-page/src/main/java/com/datrixo/crypto_datrixo_site/ico_page/conf/WebConfig.java
+++ ico-page/src/main/java/com/datrixo/crypto_datrixo_site/ico_page/conf/WebConfig.java
@@ -4,6 +4,7 @@ package com.datrixo.crypto_datrixo_site.ico_page.conf;
  * Created by Yuri Nikiforov.
  * Date: 13.08.2025
  * Time: 18:00
+ *
  **/
 
 import org.springframework.context.annotation.Configuration;
