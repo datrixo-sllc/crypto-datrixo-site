54c0311443317b7f284e23f9f9dccbe482ceefc8
datrixo-sllc <datrixosllc@gmail.com>
2025-08-19 12:28:28 +0300

    fix ico page fields errors

--- ico-page/frontend-ico-page-update/src/main.ts
+++ ico-page/frontend-ico-page-update/src/main.ts
@@ -3,6 +3,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { environment as prodEnvironment } from './environments/environment.prod';
+
 
 if (environment.production) {
   enableProdMode();
